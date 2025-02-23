c9d47b0775f824a008923a9af807a81311990104
Luka <lukabrkovic@proton.me>
2025-02-23 14:32:40 +0100

    feat: declare types

---
+++ tree/tree.c
@@ -0,0 +1,12 @@
+#include "tree.h"
+
+Tree root = {.n = {.tag = TagRoot | TagNode,
+                   .north = (Node *)&root,
+                   .west = 0,
+                   .east = 0,
+                   .path = {"/"}}};
+
+int main() {
+  printf("%p\n", &root);
+  return 0;
+}
